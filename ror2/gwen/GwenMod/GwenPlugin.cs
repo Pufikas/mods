426a09f5da8c5a1cbfea7ab4fce21d3f88ecfa5e
Pufikas <dominykas.guzys@gmail.com>
2025-02-20 21:15:27 +0200

    basic henry template for gwen making

---
+++ ror2/gwen/GwenMod/GwenPlugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using HenryMod.Survivors.Henry;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+
+namespace GwenMod
+{
+    //[BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInPlugin(MODUID, MODNAME, MODVERSION)]
+    public class GwenPlugin : BaseUnityPlugin
+    {
+        public const string MODUID = "com.pufikas.GwenMod";
+        public const string MODNAME = "Gwen";
+        public const string MODVERSION = "0.0.1";
+        public const string DEVELOPER_PREFIX = "PUFIKAS";
+
+        public static GwenPlugin instance;
+
+        void Awake()
+        {
+            instance = this;
+
+            Log.Init(Logger);
+
+            // used when you want to properly set up language folders
+            Modules.Language.Init();
+
+            // character initialization
+            new GwenSurvivor().Initialize();
+
+            // make a content pack and add it. this has to be last
+            new Modules.ContentPacks().Initialize();
+        }
+    }
+}
